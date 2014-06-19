e33bd5493973630cf3ef39ef97dbb3e00e4dbda3
Yun Jeon <yunnyhaha777@gmail.com>
2014-06-19 15:16:50 -0400

    added start screen

--- CareerbuilderSiteTech.js
+++ CareerbuilderSiteTech.js
@@ -1,1 +1,52 @@
-console.log("what is your name?);
+var name = prompt("What is your name?");
+
+var startScreen = (function(input) {
+
+	var hue = 0; 
+	var direction = 1;
+	var transitioning = false;
+
+	var wasButtonDown = false;
+
+	function centerText(ctx, text, y) {
+		var measurement = ctx.measuretext(text);
+		var x = (ctx.canvas.width - measurement.width) / 2;
+		ctx.filltext(text, x, y);
+	}
+	function draw(ctx, elapse) {
+		var y= ctx.canvas.height / 2
+		var color = 'rgb(' + hue +',0,0)';
+		ctx.clearrect(0,0, ctx.canvas.width. ctx.canvas.height);
+
+		ctx.fillstyle = 'white';
+		ctx.font = '48px monospace';
+		centertext(ctx, 'CB Site Technology Teams', y);
+
+		ctx.fillstyle = 'color';
+		ctx.font = '24px monospace';
+		centertext(ctx, 'click to begin', y + 30);
+	}
+
+	function update () {
+		hue += 1 * direction;
+		if (hue > 255) direction = -1;
+		if (hue < 0) direction = 1;
+
+		var isbuttondown = input. isbuttondown();
+
+		var mousejustclicked = !isbuttondown && wasButtonDown;
+
+		if (mousejustclicked && !transitioning) {
+			transitioning = true;
+		}
+
+		wasButtonDown = isbuttondown;
+
+		return {
+			draw: draw,
+			update: update
+		};
+
+	}
+	
+}());
